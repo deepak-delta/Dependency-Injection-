b24f84374a6ca45ceab4a1ab7061f6be1ff39717
deepak-delta <ddk3857@gmail.com>
2022-08-17 20:14:21 +0530

    DI Final

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -5,7 +5,9 @@ import { PowerService } from '../power/power.service';
 export class CpuService {
   constructor(private powerService: PowerService) {}
 
-  supplyPower(watts: number) {
-    this.powerService.supplyPower(watts);
+  compute(a: number, b: number) {
+    console.log(`Computing ${a} + ${b}`);
+    this.powerService.supplyPower(a + b);
+    return a + b;
   }
 }
